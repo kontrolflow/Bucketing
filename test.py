d923358d6b5ddaa56c2680a3ec861b31005ed905
dev <jullianabinsay@gmail.com>
2018-04-14 00:50:32 -0400

    Test Revision

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
 print("Hello World")
+
+# Bucketing
+#Jullian and I use python to basically build the bucket sort algorithm (or attempt to.... we kinda know what we're doing, ha.)
+
+#example array (working with single digits for now)
+array = [1,5,4,6]
+
+#we create empty an empty list
+bucket = []
+
+#We make an empty dictionary
+bucket2 = {}
+
+
+#We set up a for loop to iterate all the elements "tagging" them into place.
+for n in array:
+    bucket2[n] = n
+
+#Print the sorted list
+print(bucket2.values())
